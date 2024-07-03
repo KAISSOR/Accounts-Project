18ea0fed3cbd61685e09ebc42e8871cf4949727e
K A I S E R <153696501+KAISSOR@users.noreply.github.com>
2024-07-03 16:39:49 +0300

    backend/db/db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const db = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect("")
+        await mongoose.connect("mongodb+srv://kaiser:kaiser_22_h@cluster0.2fgmljk.mongodb.net/")
         console.log('Db Connected')
     } catch (error) {
         console.log('DB Connection Error');
